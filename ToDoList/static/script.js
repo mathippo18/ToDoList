3e6e23417656bcb61c77422a14317a5c44ea69ea
Mathéo POINSOT <107137963+mathippo18@users.noreply.github.com>
2023-06-09 10:11:15 +0200

    C'est un loupé de transfert

--- ToDoList/static/script.js
+++
@@ -1,7 +0,0 @@
-// This has weird behavior with parent and child interactions
-
-// [...document.querySelectorAll(".li")].forEach((item) => {
-// 	item.addEventListener("click", function () {
-// 		item.classList.toggle("done");
-// 	});
-// });
