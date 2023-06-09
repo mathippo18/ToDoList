6db9aa5856dc74c23a390c277468cfc526d59b62
Mathéo POINSOT <107137963+mathippo18@users.noreply.github.com>
2023-06-09 10:11:44 +0200

    Add files via upload

---
+++ static/script.js
@@ -0,0 +1,7 @@
+// This has weird behavior with parent and child interactions
+
+// [...document.querySelectorAll(".li")].forEach((item) => {
+// 	item.addEventListener("click", function () {
+// 		item.classList.toggle("done");
+// 	});
+// });
